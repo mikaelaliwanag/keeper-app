b2eecd3e937f0cded71955d121a9afeb11d12269
mikaelaliwanag <mikaela301@gmail.com>
2020-09-09 20:01:32 -0700

    Update App.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,47 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateNote from "./CreateNote";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  ////add note////
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+
+  }
+
+  ////delete note////
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((note) => (
-        <Note 
-          key={note.id} 
-          title={note.title} 
-          content={note.content} 
-        />
-      ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      
       <Footer />
     </div>
   );
 }
 
-export default App; 
+export default App;
